d08c1066a08d1a7aec6bc9be660643dba379b18e
Vulps <ajmcallister5@gmail.com>
2024-02-20 18:15:06 +0000

    Stop refreshing page to check authentication

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,9 +1,16 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
+import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService)
 
-  return authService.isLoggedIn();
+  if (authService.isLoggedIn()) {
+    return true;
+  } else {
+    return authService.authenticate().pipe(
+      map((data) => data.response === 'success')
+    );
+  }
 };
