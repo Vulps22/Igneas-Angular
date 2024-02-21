8f7f793d708c031c8f0a2ac5ae5b1f00219b52fc
Vulps <ajmcallister5@gmail.com>
2024-02-21 14:56:58 +0000

    Fixed: Navbar not working

--- src/app/navbar/navbar-member/navbar-member.component.ts
+++ src/app/navbar/navbar-member/navbar-member.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-member',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar-member.component.html',
   styleUrl: './navbar-member.component.css'
 })
